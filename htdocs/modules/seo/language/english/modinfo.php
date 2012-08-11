da241d2127e32af4f3352883f2f2e01cd7317838
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- htdocs/modules/seo/language/english/modinfo.php
+++ htdocs/modules/seo/language/english/modinfo.php
@@ -1,7 +1,7 @@
 <?php
 
 	// XOOPS Version
-	define('_MI_SEO_NAME', 'SEO URL Rewrite');
-	define('_MI_SEO_DESC', 'This module is for rewriting URL of any module to clean URL, modules require plugin to be written!');
+	define('_MI_SEO_NAME','SEO URL Rewrite');
+	define('_MI_SEO_DESC','This module is for rewriting URL of any module to clean URL, modules require plugin to be written!');
 	
 ?>
